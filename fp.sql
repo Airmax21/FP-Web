128a7653576583ecaab97210fe3957db1971b603
Airmax21 <75207593+Airmax21@users.noreply.github.com>
2022-07-02 21:43:10 +0700

    Add files via upload

--- fp.sql
+++ fp.sql
@@ -1,11 +1,53 @@
-create table episode(
-    movieID int NOT NULL PRIMARY KEY,
-    movieName VARCHAR (200) NOT NULL,
-    movieDesc VARCHAR (200) NOT NULL,
-    movieRate INT NOT NULL,
-    movieWatch int not null,
-    movieGenre varchar (200),
-    movieCoverLandscape varchar(MAX),
-    movieCoverPotrait varchar(MAX),
-    thumbnail varchar(MAX)
+create database dbFP;
+use dbFP
+create table users(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    pw varchar (200) NOT NULL,
+    email varchar (200) NOT NULL,
+    username varchar (200),
+    CONSTRAINT UC_user UNIQUE (id,username,email),
+    INDEX(pw,email)
+);
+create table video(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    descr VARCHAR(200) NOT NULL,
+    rate INT NOT NULL,
+    watch int not null,
+    path varchar(1000) not null,
+    CoverLandscape varchar(1000),
+    CoverPotrait varchar(1000),
+    INDEX(name,CoverLandscape,CoverPotrait)
+);
+
+create table genre(
+    movieID int not null,
+    genre varchar(1000) not null,
+    FOREIGN KEY (movieID) REFERENCES video(id)
+);
+create table favorite(
+    users int not null,
+    movieID int NOT NULL,
+    FOREIGN KEY (users) REFERENCES users(id),
+    FOREIGN KEY (movieID) REFERENCES video(id)
+);
+
+create table watchlist(
+    users int not null,
+    movieID int NOT NULL,
+    FOREIGN KEY (users) REFERENCES users(id),
+    FOREIGN KEY (movieID) REFERENCES video(id)
+);
+
+create table recent(
+    users int not null,
+    movieID int NOT NULL,
+    lastTime datetime default now(),
+    FOREIGN KEY (users) REFERENCES users(id),
+    FOREIGN KEY (movieID) REFERENCES video(id)
+);
+
+create table theAdmin(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    pw VARCHAR (10)
 );
